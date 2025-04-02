18675d92217399612cecde14b6246c5f71ef3272
minseung-gang <strause1@nate.com>
2025-04-03 01:13:34 +0900

    chore: 필요없는 state 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react'
- 
 import AppRouter from './router/routes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-     <AppRouter/>
-  )
+  return <AppRouter />
 }
 
 export default App
